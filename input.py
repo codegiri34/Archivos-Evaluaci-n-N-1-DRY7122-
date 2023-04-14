7efb66ca3c2ac5f1a64979687b3c8f58329d9fc5
Codegiri34 <benjaminaliaga800@gmail.com>
2023-04-14 22:13:15 +0000

    add datosjson.py to archivos

---
+++ input.py
@@ -0,0 +1,9 @@
+nombre = input(str("Ingrese su nombre: "))
+apellido = input(str("Ingrese su apellido: "))
+seccion = input(str("Ingrese su seccion: "))
+sede = input(str("Ingrese su sede: "))
+
+print("Su nombre: ", nombre)
+print("Su Apellido: ", apellido)
+print("Su Seccion: ", seccion)
+print("Su Sede: ", sede)
